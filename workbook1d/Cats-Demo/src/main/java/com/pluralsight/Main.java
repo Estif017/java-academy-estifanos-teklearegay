f894bdea66bed8222bc7ea375398e5bbd63a5b6a
Estifanos Teklearegay <estifoteklearegay@gmail.com>
2025-09-24 13:48:05 -0700

    Added a basic calculator using scanner

--- workbook1d/Cats-Demo/src/main/java/com/pluralsight/Main.java
+++ workbook1d/Cats-Demo/src/main/java/com/pluralsight/Main.java
@@ -4,6 +4,30 @@ package com.pluralsight;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello Kitty!!");
+        /*
+        * favoriteColor
+favoriteArtist
+Did you eat already?
+your age*/
+        String favoriteColor = "blue";
+        String favoriteArtist = "the weekend";
+        boolean breakfast = false; boolean isHappy = true;
+        String firstName = "Estifanos";
+        String lastName = "Teklearegay";
+        String fullName = firstName + " "+ lastName;
+        System.out.println(fullName);
+        System.out.println(favoriteColor);
+        System.out.println(favoriteArtist);
+        System.out.println(breakfast);
+        favoriteColor = "Red";
+        System.out.println(favoriteColor);
+
+        if(isHappy && breakfast){
+            System.out.println("Nice Job!");
+        } else if (isHappy && !breakfast) {
+            System.out.println("You could be hungry.");
+        }else{
+            System.out.println("Do some thing about it!");
+        }
     }
 }
