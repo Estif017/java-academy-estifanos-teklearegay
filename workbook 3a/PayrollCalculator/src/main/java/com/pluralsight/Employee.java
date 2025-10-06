99b112b906c90d89e2ede48f72dce37d133f8ba3
Estifanos Teklearegay <estifoteklearegay@gmail.com>
2025-10-06 14:21:31 -0700

    Working with error handling and file readers

---
+++ workbook 3a/PayrollCalculator/src/main/java/com/pluralsight/Employee.java
@@ -0,0 +1,34 @@
+package com.pluralsight;
+
+public class Employee {
+    private int employeeId;
+    private String name;
+    private double hoursWorked;
+    private double payRate;
+
+    public Employee(int _employeeId,double _hoursWorked, double _payRate){
+        this.employeeId = _employeeId;
+        this.hoursWorked = _hoursWorked;
+        this.payRate = _payRate;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getEmployeeId() {
+        return employeeId;
+    }
+
+    public void setEmployeeId(int employeeId) {
+        this.employeeId = employeeId;
+    }
+
+    public double getGrossPay(){
+        return this.hoursWorked*this.payRate;
+    }
+}
